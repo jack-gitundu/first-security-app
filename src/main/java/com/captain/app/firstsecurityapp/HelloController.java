1453100d90a48953f04bd7a4def1d0c1055195da
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-04-19 16:23:41 +0300

    first spring security commit

---
+++ src/main/java/com/captain/app/firstsecurityapp/HelloController.java
@@ -0,0 +1,13 @@
+package com.captain.app.firstsecurityapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Spring Security Rocks!!!";
+    }
+}
